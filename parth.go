801a777c849674d00a7c61b922987060bfe3e989
Daved <daved@codemodus.com>
2015-05-28 01:59:06 -0700

    Improved Int and Float handling, and corrected error returns. Improved tests, and added example.

--- parth.go
+++ parth.go
@@ -2,8 +2,10 @@
 package parth
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"unicode"
 )
 
 // Parth holds the path for parsing.
@@ -20,49 +22,51 @@ func New(path string) *Parth {
 // specified segment as a string and a nil error.  If any error is encountered,
 // a zero value string and error are returned.
 func (p *Parth) String(i int) (string, error) {
-	c, i1, i2 := 0, 0, 0
+	c, ind0, ind1 := 0, 0, 0
 	for n := 0; n < len(p.path); n++ {
 		if p.path[n] == '/' {
 			if c == i {
 				if n+1 < len(p.path) && p.path[n+1] != '/' {
-					i1 = n + 1
+					ind0 = n + 1
 				} else {
 					break
 				}
 			}
 			if c > i {
-				i2 = n
+				ind1 = n
 				break
 			}
 			c++
 		} else if n == 0 {
 			if c == i {
-				i1 = n
+				ind0 = n
 			}
 			c++
 		} else if n == len(p.path)-1 {
 			if c > i {
-				i2 = n + 1
+				ind1 = n + 1
 			}
 			break
 		}
 	}
-	if i < 0 || i2 == 0 {
+	if i < 0 || ind1 == 0 {
 		return "", fmt.Errorf("path segment index %d does not exist", i)
 	}
-	return p.path[i1:i2], nil
+	return p.path[ind0:ind1], nil
 }
 
 // Int64 receives an int representing a path segment, and returns both the
 // specified segment as an int64 and a nil error.  If any error is encountered,
 // a zero value int64 and error are returned.
 func (p *Parth) Int64(i int) (v int64, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err = strconv.ParseInt(s, 10, 64); err == nil {
-			return v, nil
+	var s string
+	if s, err = p.String(i); err == nil {
+		if s, err = getIntString(s); err == nil {
+			if v, err = strconv.ParseInt(s, 10, 64); err == nil {
+				return v, nil
+			}
 		}
 	}
-
 	return 0, err
 }
 
@@ -70,12 +74,15 @@ func (p *Parth) Int64(i int) (v int64, err error) {
 // specified segment as an int32 and a nil error.  If any error is encountered,
 // a zero value int32 and error are returned.
 func (p *Parth) Int32(i int) (_ int32, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseInt(s, 10, 32); err == nil {
-			return int32(v), nil
+	var s string
+	var v int64
+	if s, err = p.String(i); err == nil {
+		if s, err = getIntString(s); err == nil {
+			if v, err = strconv.ParseInt(s, 10, 32); err == nil {
+				return int32(v), nil
+			}
 		}
 	}
-
 	return 0, err
 }
 
@@ -83,12 +90,15 @@ func (p *Parth) Int32(i int) (_ int32, err error) {
 // specified segment as an int16 and a nil error.  If any error is encountered,
 // a zero value int16 and error are returned.
 func (p *Parth) Int16(i int) (_ int16, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseInt(s, 10, 16); err == nil {
-			return int16(v), nil
+	var s string
+	var v int64
+	if s, err = p.String(i); err == nil {
+		if s, err = getIntString(s); err == nil {
+			if v, err = strconv.ParseInt(s, 10, 16); err == nil {
+				return int16(v), nil
+			}
 		}
 	}
-
 	return 0, err
 }
 
@@ -96,12 +106,15 @@ func (p *Parth) Int16(i int) (_ int16, err error) {
 // specified segment as an int8 and a nil error.  If any error is encountered,
 // a zero value int8 and error are returned.
 func (p *Parth) Int8(i int) (_ int8, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseInt(s, 10, 8); err == nil {
-			return int8(v), nil
+	var s string
+	var v int64
+	if s, err = p.String(i); err == nil {
+		if s, err = getIntString(s); err == nil {
+			if v, err = strconv.ParseInt(s, 10, 8); err == nil {
+				return int8(v), nil
+			}
 		}
 	}
-
 	return 0, err
 }
 
@@ -109,12 +122,15 @@ func (p *Parth) Int8(i int) (_ int8, err error) {
 // specified segment as an int and a nil error.  If any error is encountered,
 // a zero value int and error are returned.
 func (p *Parth) Int(i int) (_ int, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseInt(s, 10, 0); err == nil {
-			return int(v), nil
+	var s string
+	var v int64
+	if s, err = p.String(i); err == nil {
+		if s, err = getIntString(s); err == nil {
+			if v, err = strconv.ParseInt(s, 10, 0); err == nil {
+				return int(v), nil
+			}
 		}
 	}
-
 	return 0, err
 }
 
@@ -122,12 +138,12 @@ func (p *Parth) Int(i int) (_ int, err error) {
 // specified segment as a bool and a nil error.  If any error is encountered,
 // a zero value bool and error are returned.
 func (p *Parth) Bool(i int) (v bool, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseBool(s); err == nil {
+	var s string
+	if s, err = p.String(i); err == nil {
+		if v, err = strconv.ParseBool(s); err == nil {
 			return v, nil
 		}
 	}
-
 	return false, err
 }
 
@@ -135,12 +151,14 @@ func (p *Parth) Bool(i int) (v bool, err error) {
 // specified segment as a float64 and a nil error.  If any error is encountered,
 // a zero value float64 and error are returned.
 func (p *Parth) Float64(i int) (v float64, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseFloat(s, 64); err == nil {
-			return v, nil
+	var s string
+	if s, err = p.String(i); err == nil {
+		if s, err = getFloatString(s); err == nil {
+			if v, err = strconv.ParseFloat(s, 64); err == nil {
+				return v, nil
+			}
 		}
 	}
-
 	return 0.0, err
 }
 
@@ -148,11 +166,68 @@ func (p *Parth) Float64(i int) (v float64, err error) {
 // specified segment as a float32 and a nil error.  If any error is encountered,
 // a zero value float32 and error are returned.
 func (p *Parth) Float32(i int) (_ float32, err error) {
-	if s, err := p.String(i); err == nil {
-		if v, err := strconv.ParseFloat(s, 32); err == nil {
-			return float32(v), nil
+	var s string
+	var v float64
+	if s, err = p.String(i); err == nil {
+		if s, err = getFloatString(s); err == nil {
+			if v, err = strconv.ParseFloat(s, 32); err == nil {
+				return float32(v), nil
+			}
+		}
+	}
+	return 0.0, err
+}
+
+func getIntString(s string) (string, error) {
+	ind, l := 0, 0
+	for n := 0; n < len(s); n++ {
+		if unicode.IsDigit(rune(s[n])) {
+			if l == 0 {
+				ind = n
+			}
+			l++
+		} else {
+			if l == 0 && s[n] == '.' && unicode.IsDigit(rune(s[n+1])) {
+				return "0", nil
+			}
+			if l > 0 {
+				break
+			}
 		}
 	}
 
-	return 0.0, err
+	if l == 0 {
+		return "", errors.New("path segment does not contain int")
+	}
+	return s[ind : ind+l], nil
+}
+
+func getFloatString(s string) (string, error) {
+	c, ind, l := 0, 0, 0
+	for n := 0; n < len(s); n++ {
+		if unicode.IsDigit(rune(s[n])) {
+			if l == 0 {
+				ind = n
+			}
+			l++
+		} else if s[n] == '.' {
+			if l == 0 {
+				ind = n
+			}
+			if c > 0 {
+				break
+			}
+			l++
+			c++
+		} else {
+			if l > 0 {
+				break
+			}
+		}
+	}
+
+	if l == 0 || s[ind:ind+l] == "." {
+		return "", errors.New("path segment does not contain float")
+	}
+	return s[ind : ind+l], nil
 }
--- parth_test.go
+++ parth_test.go
@@ -1,144 +1,203 @@
 package parth_test
 
 import (
-	"strings"
+	"fmt"
 	"testing"
 
 	"github.com/codemodus/parth"
 )
 
-func TestStringComplex(t *testing.T) {
-	tests := [][]string{
-		[]string{"/test", "/nest", "/best", "/rest", "/zest", "/jest", ""},
-		[]string{"/abc-def", "/uvw_xyz/"},
-		[]string{"/", "/test"},
-		[]string{"test", "/best"},
+const (
+	errFmtGotWant = "Type = %T, Segment Value = %v, want %v"
+	errFmtExpErr  = "Did not receive expected err for segment value %v"
+	printFmt      = "Segment Index = %v , Type = %T, Value = %v\n"
+)
+
+func Example() {
+	urlPath := "/zero/1/2/nn3.3nn/4.4"
+	parthObject := parth.New(urlPath)
+
+	if seg, err := parthObject.String(0); err == nil {
+		fmt.Printf(printFmt, 0, seg, seg)
+	}
+
+	if seg, err := parthObject.Bool(1); err == nil {
+		fmt.Printf(printFmt, 1, seg, seg)
+	}
+
+	if seg, err := parthObject.Int(2); err == nil {
+		fmt.Printf(printFmt, 2, seg, seg)
+	}
+
+	if seg, err := parthObject.Float32(3); err == nil {
+		fmt.Printf(printFmt, 3, seg, seg)
+	}
+
+	if seg, err := parthObject.Int(4); err == nil {
+		fmt.Printf(printFmt, 4, seg, seg)
+	}
+
+	// Output:
+	// Segment Index = 0 , Type = string, Value = zero
+	// Segment Index = 1 , Type = bool, Value = true
+	// Segment Index = 2 , Type = int, Value = 2
+	// Segment Index = 3 , Type = float32, Value = 3.3
+	// Segment Index = 4 , Type = int, Value = 4
+
+}
+
+func TestString(t *testing.T) {
+	var tests = []struct {
+		i int
+		p string
+		r string
+		e bool
+	}{
+		{0, "/test1", "test1", false},
+		{1, "/test1/test-2", "test-2", false},
+		{2, "/test1/test-2/test_3/", "test_3", false},
+		{0, "test4/t4", "test4", false},
+		{1, "/test5//", "", true},
+		{3, "/test6", "", true},
+		{0, "//test7", "", true},
+		{0, "/", "", true},
 	}
 
 	for _, v := range tests {
-		var path string
-		for _, y := range v {
-			path += y
+		po := parth.New(v.p)
+		seg, err := po.String(v.i)
+		if err != nil && !v.e {
+			t.Fatal(err)
 		}
-		po := parth.New(path)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
+		}
 
-		for k, v := range v {
-			seg, err := po.String(k)
-			if err != nil && v != "" && v != "/" {
-				t.Fatal(err)
-			}
-
-			want := strings.TrimSuffix(strings.TrimPrefix(v, "/"), "/")
-			got := seg
-			if got != want {
-				t.Errorf("Segment Value = %v, want %v", got, want)
-			}
+		want := v.r
+		got := seg
+		if got != want {
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 }
 
 func TestInts(t *testing.T) {
-	ePath := "/error"
-	tests := []string{"/0", "/1", "/2", ePath}
+	var tests = []struct {
+		p string
+		r int
+		e bool
+	}{
+		{"/1", 1, false},
+		{"/2.2", 2, false},
+		{"/.3", 0, false},
+		{"/error", 0, true},
+	}
 
-	var path string
 	for _, v := range tests {
-		path += v
-	}
-	po := parth.New(path)
-
-	for k, v := range tests {
-		seg, err := po.Int(k)
-		if err != nil && v != ePath {
+		po := parth.New(v.p)
+		seg, err := po.Int(0)
+		if err != nil && !v.e {
 			t.Fatal(err)
 		}
-
-		want := int(k)
-		if v == ePath {
-			want = int(0)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
+
+		want := int(v.r)
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 
-	for k, v := range tests {
-		seg, err := po.Int8(k)
-		if err != nil && v != ePath {
+	for _, v := range tests {
+		po := parth.New(v.p)
+		seg, err := po.Int8(0)
+		if err != nil && !v.e {
 			t.Fatal(err)
 		}
-
-		want := int8(k)
-		if v == ePath {
-			want = int8(0)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
+
+		want := int8(v.r)
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 
-	for k, v := range tests {
-		seg, err := po.Int16(k)
-		if err != nil && v != ePath {
+	for _, v := range tests {
+		po := parth.New(v.p)
+		seg, err := po.Int16(0)
+		if err != nil && !v.e {
 			t.Fatal(err)
 		}
-
-		want := int16(k)
-		if v == ePath {
-			want = int16(0)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
+
+		want := int16(v.r)
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 
-	for k, v := range tests {
-		seg, err := po.Int32(k)
-		if err != nil && v != ePath {
+	for _, v := range tests {
+		po := parth.New(v.p)
+		seg, err := po.Int32(0)
+		if err != nil && !v.e {
 			t.Fatal(err)
 		}
-
-		want := int32(k)
-		if v == ePath {
-			want = int32(0)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
+
+		want := int32(v.r)
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 
-	for k, v := range tests {
-		seg, err := po.Int64(k)
-		if err != nil && v != ePath {
+	for _, v := range tests {
+		po := parth.New(v.p)
+		seg, err := po.Int64(0)
+		if err != nil && !v.e {
 			t.Fatal(err)
 		}
-
-		want := int64(k)
-		if v == ePath {
-			want = int64(0)
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
+
+		want := int64(v.r)
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 }
 
 func TestBool(t *testing.T) {
-	ePath := "/error"
-	var tests = []struct {
+	tests := []struct {
 		p string
 		b bool
+		e bool
 	}{
-		{"/true", true},
-		{ePath, false},
-		{"/false", false},
-		{"/0", false},
-		{"/1", true},
+		{"/1", true, false},
+		{"/t", true, false},
+		{"/T", true, false},
+		{"/true", true, false},
+		{"/TRUE", true, false},
+		{"/True", true, false},
+		{"/error", false, true},
+		{"/0", false, false},
+		{"/f", false, false},
+		{"/F", false, false},
+		{"/false", false, false},
+		{"/FALSE", false, false},
+		{"/False", false, false},
 	}
 
 	var path string
@@ -149,29 +208,38 @@ func TestBool(t *testing.T) {
 
 	for k, v := range tests {
 		seg, err := po.Bool(k)
-		if err != nil && v.p != ePath {
+		if err != nil && !v.e {
 			t.Fatal(err)
+		}
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
 
 		want := v.b
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 }
 
 func TestFloats(t *testing.T) {
-	ePath := "/error"
-	var tests = []struct {
+	tests := []struct {
 		p   string
 		f32 float32
 		f64 float64
+		e   bool
 	}{
-		{"/0.0", 0.0, 0.0},
-		{"/1.2", 1.2, 1.2},
-		{"/2", 2.0, 2.0},
-		{ePath, 0.0, 0.0},
+		{"/0.1", 0.1, 0.1, false},
+		{"/0.2a", 0.2, 0.2, false},
+		{"/aaaa1.3", 1.3, 1.3, false},
+		{"/4", 4.0, 4.0, false},
+		{"/5aaaa", 5.0, 5.0, false},
+		{"/aaa6aa", 6.0, 6.0, false},
+		{"/.7.aaaa", 0.7, 0.7, false},
+		{"/.8aa", 0.8, 0.8, false},
+		{"/error", 0.0, 0.0, true},
+		{"/.", 0.0, 0.0, true},
 	}
 
 	var path string
@@ -182,27 +250,33 @@ func TestFloats(t *testing.T) {
 
 	for k, v := range tests {
 		seg, err := po.Float32(k)
-		if err != nil && v.p != ePath {
+		if err != nil && !v.e {
 			t.Fatal(err)
+		}
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
 
 		want := v.f32
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 
 	for k, v := range tests {
 		seg, err := po.Float64(k)
-		if err != nil && v.p != ePath {
+		if err != nil && !v.e {
 			t.Fatal(err)
+		}
+		if err == nil && v.e {
+			t.Errorf(errFmtExpErr, v.p)
 		}
 
 		want := v.f64
 		got := seg
 		if got != want {
-			t.Errorf("Segment Value = %v, want %v", got, want)
+			t.Errorf(errFmtGotWant, got, got, want)
 		}
 	}
 }
